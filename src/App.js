f7ecc63c5ac8dfdb47dfdb93b04b7ed9c75efd2c
fazzy12 <kaluifeanyi00@gmail.com>
2022-12-10 12:31:10 +0100

    built search filter function

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import { useState } from 'react';
 
 function App() {
 
-  const [search, setSearch] = useState('')  
+  // Create a state variable called "search" and a function to update it
+  const [search, setSearch] = useState('');
+  
+  // Render the input field and set the value of search to the input text
   return (
     <div className="App">
-      <input type="text" className="input-text" placeholder="serach..." onChange={event =>{setSearch(event.target.value)}}/>
-      {DATA.filter((val)=>{
-        if (search === ''){
-          return val
-        }else if (val.first_name.toLowerCase().includes(search.toLowerCase())){
-          return val
+      <input type="text" className="input-text" placeholder="serach..." onChange={event => { setSearch(event.target.value); }} />
+
+      {/* Filter the DATA array based on the search text and render a div element for each item */}
+
+      {DATA.filter((val) => {
+        if (search === '') {
+          return val;
+        } else if (val.first_name.toLowerCase().includes(search.toLowerCase())) {
+          return val;
         }
-      }).map((val, key)=>{
+      }).map((val, key) => {
         return <div className='user' key={key}>
-        <p>{val.first_name}</p>
-        </div>
+          <p>{val.first_name}</p>
+        </div>;
       })}
     </div>
   );
